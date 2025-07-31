8565130c0c4b3d1f7b5c7066beabdc865007a52d
zaibk <zaibkm@gmail.com>
2025-07-31 14:22:01 -0400

    This is my second commit

---
+++ ReadWrite.py
@@ -0,0 +1,48 @@
+#This is out reading and writing to only text files
+# always close an openedfile
+
+from fileinput import close
+
+#file = open('test.txt')  #give file path in brackets and store this in a object name file
+#print(file.read()) #will read all contents of file and print in output
+#print(file.read(5)) #this will read first 5 char of the file
+
+#inorder to read one complete line
+#print(file.readline()) #will read first line
+#print(file.readline()) #will read second line
+
+#in order to read 100s of lines you need to use a while loop
+#line = file.readline()
+#while line != "":
+ #   print(line)
+  #  line = file.readline()
+
+#there is a readlines() method which
+#1. reads all the lines
+#2. stores each line as an entry of a list, returns them as strings of a list
+# [abc, banana, cat, dog, elephant, mouse]
+#3. use for loops
+
+#for i in file.readlines():
+ #   print(i)
+
+#file.close() #close file
+
+
+#file = open('test.txt')
+#print(file.read(6))
+#print(file.readline())
+
+
+#line = file.readline()
+
+#while line != "":
+ #   print(line)
+  #  line = file.readline()
+
+
+
+#for i in file.readlines():
+ #   print(i)
+
+#file.close()
